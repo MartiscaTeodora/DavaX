1dc95d923c226fe6b786df7964e87da5249b7740
Teodora Maria Martisca <Teodora-Maria.Martisca@endava.com>
2026-04-09 16:55:28 +0300

    ingest+ chroma db

---
+++ Module 3/tema/ingest.py
@@ -0,0 +1,43 @@
+import json
+import chromadb
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+
+print(os.getcwd())
+# load env
+load_dotenv()
+
+# init OpenAI
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+# init ChromaDB
+chroma_client = chromadb.PersistentClient(path="./chroma_db")
+collection = chroma_client.get_or_create_collection(name="books")
+
+# load books
+base_dir = os.path.dirname(__file__)
+file_path = os.path.join(base_dir, "books.json")
+
+with open(file_path, "r", encoding="utf-8") as f:
+    books = json.load(f)
+# ingest
+for i, book in enumerate(books):
+    text = f"{book['title']}. {book['summary']}"
+
+    response = client.embeddings.create(
+        model="text-embedding-3-small",
+        input=text
+    )
+
+    embedding = response.data[0].embedding
+
+    collection.add(
+        ids=[str(i)],
+        embeddings=[embedding],
+        documents=[book["summary"]],
+        metadatas=[{"title": book["title"]}]
+    )
+
+print(" Ingest completed successfully")
